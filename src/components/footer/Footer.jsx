3e35473a1f55a082a7e4d2f6836aa98b319d62f5
Ivan1990Nik <pelevin.vanyusha90@mail.ru>
2025-09-08 21:05:56 +0500

    homepages3

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,51 @@
-import "./footer.css"
+import "./footer.css";import { FaTelegramPlane, FaWhatsapp, FaVk } from 'react-icons/fa';
+
+
+
+
 const Footer = () => {
   return (
-<div className='footer-container'>Footer</div>
-)
+    <div className="footer-container">
+  
+
+
+      <p>с нами удобно связаться 👉</p>
+      <div className="links-container">
+        <a 
+          href="https://t.me/your_telegram_channel" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="social-link telegram"
+        >
+        <FaTelegramPlane />
+        </a>
+        <a 
+          href="https://wa.me/1234567890"  // Замени 1234567890 на свой номер
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="social-link whatsapp"
+        >
+         <FaWhatsapp />
+        </a>
+        <a 
+          href="https://www.torproject.org/"  // Или ссылка на ваш onion-сайт, напр. http://youronion.onion
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="social-link tor"
+        >
+          Max
+        </a>
+        <a 
+          href="https://vk.com/your_vk_page" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="social-link vkontakte"
+        >
+         <FaVk />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
